1f770f203acb5606459fbb287fcb01f2d557305f
Guru Nagajothi S <gurunagajothi2007@gmail.com>
2026-01-09 21:29:23 +0530

    Update auth.js

--- js/auth.js
+++ js/auth.js
@@ -1,25 +1,13 @@
-document.getElementById("loginForm").addEventListener("submit", function (e) {
-  e.preventDefault();
-
-  const email = email.value;
-  const password = password.value;
-
-  if (!email.endsWith("@gmail.com")) {
-    alert("Please use a valid Gmail ID");
-    return;
-  }
-
-  if (password.length < 6) {
-    alert("Password must be at least 6 characters");
-    return;
-  }
-
-  localStorage.setItem("loggedInUser", email);
-  window.location.href = "dashboard.html";
-});
-
 function socialLogin(platform) {
-  alert(`Redirecting to ${platform} login (Simulation)`);
-  localStorage.setItem("loggedInUser", platform + " User");
-  window.location.href = "dashboard.html";
+  // show fake loading
+  alert(`Connecting to ${platform}...`);
+
+  // simulate login delay
+  setTimeout(() => {
+    localStorage.setItem("loggedInUser", platform + " User");
+    localStorage.setItem("loginType", platform);
+
+    // redirect
+    window.location.href = "dashboard.html";
+  }, 1200);
 }
