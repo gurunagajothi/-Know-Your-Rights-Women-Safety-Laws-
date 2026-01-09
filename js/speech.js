bc49508da1f8798e8fc91ee5e79990e3ea05d0e9
Guru Nagajothi S <gurunagajothi2007@gmail.com>
2026-01-09 21:18:58 +0530

    Update speech.js

--- js/speech.js
+++ js/speech.js
@@ -1,18 +1,21 @@
 let synth = window.speechSynthesis;
 let utterance;
+let currentLang = "en-IN";
+
+function setLanguage(lang) {
+  if (lang === "en") currentLang = "en-IN";
+  if (lang === "ta") currentLang = "ta-IN";
+  if (lang === "hi") currentLang = "hi-IN";
+}
 
 function startReading() {
   stopReading();
   const text = document.getElementById("lawsContent").innerText;
   utterance = new SpeechSynthesisUtterance(text);
-  utterance.lang = "en-IN";
+  utterance.lang = currentLang;
   synth.speak(utterance);
 }
 
-function pauseReading() {
-  if (synth.speaking) synth.pause();
-}
-
 function stopReading() {
-  synth.cancel();
+  if (synth.speaking) synth.cancel();
 }
